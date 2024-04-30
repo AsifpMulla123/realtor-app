1bb5d75cc2d23cd5b0e5aa8e8a14f9aba1e1d243
Asif <ashupmulla@gmail.com>
2024-04-30 19:41:35 +0530

    creating a private route and custom hook for protecting profile page and adding logout functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { getAuth } from "firebase/auth";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const Profile = () => {
+export default function Profile() {
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+  const { name, email } = formData;
+  function onLogout() {
+    auth.signOut();
+    navigate("/");
+  }
   return (
-    <div>Profile</div>
-  )
+    <>
+      <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
+        <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
+        <div className="w-full md:w-[50%] mt-6 px-3">
+          <form>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              disabled
+              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mb-6"
+            />
+            <input
+              type="email"
+              id="email"
+              value={email}
+              disabled
+              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mb-6"
+            />
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+              <p className="flex items-center mb-6">
+                Do you want to change your name?
+                <span className="text-red-600 cursor-pointer hover:text-red-700 ml-1 transition ease-in-out">
+                  Edit
+                </span>
+              </p>
+              <p
+                className="text-blue-600 cursor-pointer hover:text-red-800 transition ease-in-out duration-200"
+                onClick={onLogout}
+              >
+                Sign out
+              </p>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
+  );
 }
-
-export default Profile
