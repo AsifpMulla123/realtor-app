62a3f41ed5330c73be4041b9863faa3f5bc963c4
Asif <ashupmulla@gmail.com>
2024-04-30 11:33:45 +0530

    complete OAuth functionality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,13 +1,47 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const OAuth = () => {
+  const navigate = useNavigate();
+  async function onGoogleClick() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      // Check for the user
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      console.log(user);
+      navigate("/");
+    } catch (error) {
+      toast.error("Could not authorize with Google");
+      console.log(error);
+    }
+  }
   return (
-    <button className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 rounded uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md transition duration-150 ease-in-out">
-      <FcGoogle className="text-xl bg-white rounded-full mr-2"/>
+    <button
+      className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 rounded uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md transition duration-150 ease-in-out"
+      onClick={onGoogleClick}
+      type="button"
+    >
+      <FcGoogle className="text-xl bg-white rounded-full mr-2" />
       continue with Google
     </button>
-  ); 
+  );
 };
 
 export default OAuth;
