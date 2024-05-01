b2889af67409bea349bf09bcd0652eb7fe2461fe
Asif <ashupmulla@gmail.com>
2024-05-01 10:55:52 +0530

    creating spinner component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,6 @@ import { useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
   const location = useLocation();
-  console.log(location.pathname);
   const [pageState, setPageState] = useState("Sign in");
   const navigate = useNavigate();
   const auth = getAuth();
@@ -24,7 +23,7 @@ const Header = () => {
     }
   }
   return (
-    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+    <div className="bg-white border-b shadow-sm sticky top-0 z-40">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
           <img
